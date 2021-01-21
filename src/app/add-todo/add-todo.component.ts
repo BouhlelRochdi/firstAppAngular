89e4f9a9ed415184e62b9c1c6587041b432884fc
Bouhlel Rochdi <rochdi.bouhlel@hotmail.fr>
2021-01-21 11:15:21 +0100

    test

--- src/app/add-todo/add-todo.component.ts
+++ src/app/add-todo/add-todo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormControlDirective, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-add-todo',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-todo.component.css']
 })
 export class AddTodoComponent implements OnInit {
+  addTodoForm : FormGroup;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.addTodoForm = new FormGroup({
+      name: new FormControl(''),
+      description: new FormControl('')
+    });
+  }
+
+  save(){
+    console.log(this.addTodoForm.value);
+    
   }
 
 }
