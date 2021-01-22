83d3f818f77afd2090265e995e89136a2cf4a379
Rochdi Bouhlel <75728153+BouhlelRochdi@users.noreply.github.com>
2021-01-22 14:47:12 +0100

    Update add-todo.component.ts

--- src/app/add-todo/add-todo.component.ts
+++ src/app/add-todo/add-todo.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormControlDirective, FormGroup, Validators } from '@angular/forms';
-
+import { Component, NgModule, OnInit } from '@angular/core';
+import { FormControl, FormControlDirective, FormGroup, Validators } from '@angular/forms';
+import { from } from 'rxjs';
+import {ReactiveFormsModule} from '@angular/forms';
 @Component({
   selector: 'app-add-todo',
   templateUrl: './add-todo.component.html',
@@ -14,29 +15,17 @@ export class AddTodoComponent implements OnInit {
   ngOnInit(): void {
     this.addTodoForm = new FormGroup({
       name: new FormControl('', [Validators.required, Validators.minLength(8)]),
-      description: new FormControl('',[Validators.required, Validators.minLength(8)])
+      lastname : new FormControl('', Validators.requiredTrue),
+      description: new FormControl('', [Validators.required, Validators.maxLength(8)])
+
     });
     
-  }
- 
-  save(){
-    console.log(this.addTodoForm.value);
-    console.log(this.addTodoForm.controls);
-}
-nameError() {
-
-  const name = this.addTodoForm.controls.name;
-
-  return name.touched && name.hasError('required');
-
-}
-descriptionError() {
-
-  const description = this.addTodoForm.controls.description;
-
-  return description.touched && description.hasError('required');
-
-}
-
     
   }
+  save(){
+    console.log(this.addTodoForm.value);
+    console.log(this.addTodoForm.controls['name'].value);
+    }
+  }
+
+ 
