91564af5f2872db001db1b7579efd053814b7ca1
Rochdi Bouhlel <75728153+BouhlelRochdi@users.noreply.github.com>
2021-01-28 19:06:17 +0100

    Update add-todo.component.ts

--- src/app/add-todo/add-todo.component.ts
+++ src/app/add-todo/add-todo.component.ts
@@ -9,22 +9,38 @@ import {ReactiveFormsModule} from '@angular/forms';
 })
 export class AddTodoComponent implements OnInit {
   addTodoForm : FormGroup;
+  a = JSON.parse(localStorage.getItem('add-todo')) || [];
 
   constructor() { }
 
   ngOnInit(): void {
     this.addTodoForm = new FormGroup({
       name: new FormControl('', [Validators.required, Validators.minLength(8)]),
+      lastname : new FormControl('', Validators.required),
       description: new FormControl('', [Validators.required, Validators.maxLength(8)])
-
     });
     
-    
   }
-  save(){
+
+  getAddtodo(){
+    let todo = {
+      name: this.addTodoForm.controls['name'].value,
+      desc: this.addTodoForm.controls['description'].value
+    }
+    this.a.push(todo);
+    alert(todo);
+    localStorage.setItem('add-todo', JSON.stringify(this.a));
+  }  
+
+  test(){
     console.log(this.addTodoForm.value);
     console.log(this.addTodoForm.controls['name'].value);
     }
+   
+  onSubmit(){
+    console.log(this.addTodoForm.value);
+  }
+    
   }
 
  
